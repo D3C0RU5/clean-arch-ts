1a99a0778433ba707309451c4f2e67c15f30e509
che <che@certi.org.br>
2024-03-27 23:59:23 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -37,6 +37,9 @@ module.exports = {
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
 
+  // A preset that is used as a base for Jest's configuration
+  preset: '@shelf/jest-mongodb',
+
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
   //   "json",
@@ -102,9 +105,6 @@ module.exports = {
 
   // An enum that specifies notification mode. Requires { notify: true }
   // notifyMode: "failure-change",
-
-  // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
 
   // Run tests from one or more projects
   // projects: undefined,
