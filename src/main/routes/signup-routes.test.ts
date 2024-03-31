50c79f35bfa458d02476eca8b59698ea08f340f8
che <che@certi.org.br>
2024-03-31 13:49:42 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,17 +1,29 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    MongoHelper.connect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection?.deleteMany({})
+  })
+
+  afterAll(async () => {
+    MongoHelper.disconnect()
+  })
+
   it('Return an account on success', async () => {
     // Act
-    const response = await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Taynara',
-        email: 'taynarinha@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
+    const response = await request(app).post('/api/signup').send({
+      name: 'Taynara',
+      email: 'taynarinha@gmail.com',
+      password: '123',
+      passwordConfirmation: '123',
+    })
 
     // Assert
     expect(response.status).toBe(200)
