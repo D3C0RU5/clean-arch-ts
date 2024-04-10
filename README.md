bd03346ce40490fb6db3adb451f8ff61ffa6636f
che <che@certi.org.br>
2024-04-09 23:59:09 -0300

    feat: ensure MongoDb reconnects if connetion is down, and add Dockerfile to node

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jetmoney-api
+
+## How to build the image
+
+Execute the command `docker build -t jetmoney-api .`
+
+## How to start the container
+
+docker container run -d -p 5050:5050 jetmoney-api
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,11 +2,13 @@ import { Collection, Db, Document, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient | null,
+  uri: '' as string,
   db: null as Db | null,
   async connect(
     uri: string = process.env.MONGO_URL || '',
     db_name: string = process.env.MONGO_DB_NAME || '',
   ): Promise<void> {
+    this.uri = uri
     this.client = new MongoClient(uri)
     this.db = this.client.db(db_name)
   },
@@ -15,8 +17,11 @@ export const MongoHelper = {
     this.client = null
     this.db = null
   },
-  getCollection(name: string): Collection | undefined {
-    return this.db?.collection(name)
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.db || !this.client) {
+      await this.connect(this.uri)
+    }
+    return this.db!.collection(name)
   },
   map(document: Document): Document {
     const { _id, documentWithoutId } = document
