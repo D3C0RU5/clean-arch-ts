b12dd198023af4418cb162e01916bd5bbb8bdcbd
che <che@certi.org.br>
2024-04-14 16:37:38 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -13,15 +13,29 @@ const makeSut = (): SutType => {
 
 describe('Login Controller', () => {
   it('Return 400 if no email is provided', async () => {
+    // Arrange
     const { sut } = makeSut()
-
     const httpRequest = {
       body: { password: 'any_password' },
     }
 
+    // Act
     const httpResponse = await sut.handle(httpRequest)
 
     // Assert
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+  it('Return 400 if no password is provided', async () => {
+    // Arrange
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: { email: 'any_email@mail.com' },
+    }
+
+    // Act
+    const httpResponse = await sut.handle(httpRequest)
+
+    // Assert
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 })
