45435d12e23d76668a882d45e305ba16124796ae
che <che@certi.org.br>
2024-04-14 18:59:23 -0300

    test: ensure RequiredField Validation returns null if validation succeeds

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,32 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = () => {
+  const sut = new RequiredFieldValidation('field')
+
+  return { sut }
+}
+
+describe('RequiredField Validation', () => {
+  it('Return n MissingParamError if validation fails', () => {
+    // Arrange
+    const { sut } = makeSut()
+
+    // Act
+    const error = sut.validate({})
+
+    // Assert
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  it('Return null if validation successeds', () => {
+    // Arrange
+    const { sut } = makeSut()
+
+    // Act
+    const error = sut.validate({ field: 'any_value' })
+
+    // Assert
+    expect(error).toEqual(null)
+  })
+})
