ddd5c37216a11c2997116dae9319ab964dc4c5b5
che <che@certi.org.br>
2024-04-14 16:48:25 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,14 +1,27 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { EmailValidator } from '../../protocols/email-validator'
 import { LoginController } from './login'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
 
 type SutType = {
   sut: LoginController
+  emailValidatorStub: EmailValidator
 }
 
 const makeSut = (): SutType => {
-  const sut = new LoginController()
-  return { sut }
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new LoginController(emailValidatorStub)
+  return { sut, emailValidatorStub }
 }
 
 describe('Login Controller', () => {
@@ -25,6 +38,7 @@ describe('Login Controller', () => {
     // Assert
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
   it('Return 400 if no password is provided', async () => {
     // Arrange
     const { sut } = makeSut()
@@ -37,5 +51,20 @@ describe('Login Controller', () => {
 
     // Assert
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  it('Call EmailValidator with correct email', async () => {
+    // Arrange
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: { email: 'any_email@mail.com', password: 'any_password' },
+    }
+
+    // Act
+    await sut.handle(httpRequest)
+
+    // Assert
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,15 +1,26 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../../protocols/email-validator'
 
 export class LoginController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body
+
+    if (!email) {
       return badRequest(new MissingParamError('email'))
     }
-    if (!httpRequest.body.password) {
+
+    if (!password) {
       return badRequest(new MissingParamError('password'))
     }
+
+    if (!this.emailValidator.isValid(email)) {
+      return badRequest(new InvalidParamError('email'))
+    }
+
     return ok({})
   }
 }
