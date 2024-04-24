1d7fdc0f966e2f934dbbbbf067ae6d510d0a19d3
che <che@certi.org.br>
2024-04-23 23:28:22 -0300

    feat: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  },
+}))
+
 describe('Jwt Adapter', () => {
   it('Call sign with correct params', async () => {
     const sut = new JwtAdapter('secret')
@@ -8,5 +14,13 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id')
 
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  it('Return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+
+    const accessToken = await sut.encrypt('any_id')
+
+    expect(accessToken).toBe('any_token')
   })
 })
