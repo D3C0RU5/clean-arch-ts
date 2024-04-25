32e6aa70238e112210a3655da02fa396425e5394
che <che@certi.org.br>
2024-04-24 21:48:30 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,5 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:root@localhost:27017',
   port: process.env.port || 5050,
   mongoDbName: 'jetmoney',
+  jwtSecret: process.env.JWT_SECRET || 'c5565254-03be-4217-bcd3-3339129ec5b0',
 }
