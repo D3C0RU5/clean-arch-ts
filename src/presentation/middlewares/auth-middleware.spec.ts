80466864792fc00d4e107908c34ee7b9c0f39ed3
D3C0RU5 <chpeterle.profissional@gmail.com>
2024-05-31 19:09:03 -0300

    refactor: move AuthMiddleware dependences to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,11 @@
-import { HttpRequest } from '../protocols'
+import {
+  HttpRequest,
+  LoadAccountByToken,
+  AccountModel,
+} from './auth-middleware-protocols'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
-import { AccountModel } from '../../domain/models/account'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
 const makeFakeAccount = (): AccountModel => ({
   email: 'valid_email@mail.com',
